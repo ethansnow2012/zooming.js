826a6afcb62c6daf4f0c44e327734a162dbd5dd7
Ethansnow2012 <ethansnow2012@gmail.com>
2022-06-04 19:01:21 +0800

    feat: developing zoom for El in document flow- part3 - working

--- zooming.ts
+++ zooming.ts
@@ -26,14 +26,14 @@ enum frameOperations {
 }
 
 enum frameMetas {
-    framemetaY= "framemeta-y",
-    framemetaX= "framemeta-x",
+    framemetaY = "framemeta-y",
+    framemetaX = "framemeta-x",
     framemetaWidth = "framemeta-width",
     framemetaHeight = "framemeta-height"
 }
 enum frameMetas$map$dimensionKey {
-    framemetaY= "top",
-    framemetaX= "left",
+    framemetaY = "top",
+    framemetaX = "left",
     framemetaWidth = "width",
     framemetaHeight = "height"
 }
@@ -53,6 +53,10 @@ type fullHtmlDimensions = {
 interface ZoomingPrototype$helper {
     toggle(target: HTMLElement): void
 }
+
+interface ZoomingPrototype$innerUtils {
+    stringUnitsRemove(str: string): string
+}
 interface ZoomingPrototype {
     getRectContext(target: HTMLElement): RectContext,
     toggleZoomInOut(targetFrame: HTMLElement, operation: frameOperations): void,
@@ -61,15 +65,21 @@ interface ZoomingPrototype {
     getDynamicRootClientRect(HTMLElement): DOMRect,
     createDummyDiv(): HTMLDivElement,
     insertDummy(element: HTMLElement, target: Element, style: fullHtmlDimensions): HTMLDivElement,
-    setInitialCssAttribute(target: Element, dimension: fullHtmlDimensions): void
+    removeDummy(target: Element): void,
+    setInitialCssAttribute(target: Element, dimension: fullHtmlDimensions): void,
+    toggleZoomInOut$Deferred(targetFrame: HTMLElement, operation: frameOperations, aa?: RectContext): void,
+    isRelativeBlock(target: Element)
 }
 interface ZoomingInnerThis {
     rootClientRect: DOMRect,
     styleNotAbsolute: typeof styleNotAbsolute,
     cssClasses: typeof cssClasses,
     helper: ZoomingPrototype$helper,
+    innerUtils: ZoomingPrototype$innerUtils,
     relativeFrameInRegister: WeakMap<HTMLElement, any>,// typing needed
-    relativeFrameOutRegister: WeakMap<HTMLElement, any>// typing needed
+    relativeFrameOutRegister: WeakMap<HTMLElement, any>,// typing needed
+    dummyDiv: WeakMap<HTMLElement, any>,
+    endTransistionHandle: WeakMap<HTMLElement, any>,
 }
 interface Zooming extends ZoomingPrototype, ZoomingInnerThis { }
 
@@ -78,6 +88,8 @@ interface Zooming extends ZoomingPrototype, ZoomingInnerThis { }
 function _zooming(this: Zooming): void {
     this.relativeFrameInRegister = new WeakMap()
     this.relativeFrameOutRegister = new WeakMap()
+    this.dummyDiv = new WeakMap()
+    this.endTransistionHandle = new WeakMap()
     this.cssClasses = cssClasses
     this.styleNotAbsolute = styleNotAbsolute
     this.helper = {
@@ -95,6 +107,11 @@ function _zooming(this: Zooming): void {
             }
         }
     }
+    this.innerUtils = {
+        stringUnitsRemove: (str)=>{
+            return str.replace('pxpx', 'px')
+        }
+    }
 }
 _zooming.prototype.getDynamicRootClientRect = function (target) {
     return target.closest('.zooming-frame-wrapper').getBoundingClientRect();
@@ -110,41 +127,90 @@ _zooming.prototype.getRectContext = function (target): RectContext {
     }
 }
 
-_zooming.prototype.createDummyDiv = function() {
+_zooming.prototype.createDummyDiv = function () {
     var div = document.createElement('div');
     return div;
 }
 
-_zooming.prototype.setInitialCssAttribute = function(target, dimensions){
+_zooming.prototype.setInitialCssAttribute = function (target, dimensions) {
     console.log(frameMetas)
-    
-    for( let x in frameMetas){
+
+    for (let x in frameMetas) {
         let value = dimensions[frameMetas$map$dimensionKey[x]]
         target.dataset[x] = value
         console.log(x)
     }
 }
 
-_zooming.prototype.insertDummy = function(element, target, {width, height, top, left}:fullHtmlDimensions) {
+_zooming.prototype.insertDummy = function (element, target, { width, height, top, left }: fullHtmlDimensions) {
+    const _this: Zooming = this
+    _this.dummyDiv.set(target, element)
     element.style.position = "relative"
-    element.style.width = width+'px'
-    element.style.height = height+'px'
-    // element.style.top = top+'px'
-    // element.style.left = left+'px'
-    
+    element.style.width = width + 'px'
+    element.style.height = height + 'px'
+
     element.classList.add(cssClasses.dummyDiv)
 
-    target.parentNode.insertBefore(element, target)
+    target.parentNode.insertBefore(element, target)//parentNode => not flexable
+}
+_zooming.prototype.removeDummy = function(target){
+    const _this: Zooming = this
+    const dummyTobeRemoved = _this.dummyDiv.get(target)
+    //dummyTobeRemoved.parentNode
+    dummyTobeRemoved.remove()
 }
 
-//_zooming.prototype.becomeAbsolute = function()
+
+_zooming.prototype.isRelativeBlock = function (targetFrame: HTMLElement) {
+    const _this: Zooming = this
+    const calcPosition = window.getComputedStyle(targetFrame).position
+    const isRelativeBlock = calcPosition === _this.styleNotAbsolute.emply
+        || calcPosition === _this.styleNotAbsolute.relative
+        || _this.relativeFrameInRegister.has(targetFrame)
+    return isRelativeBlock
+}
+
+//deferred for render life cycle
+_zooming.prototype.toggleZoomInOut$Deferred = function (targetFrame: HTMLElement, operation: frameOperations, rectContext: RectContext | undefined) {
+    const _this: Zooming = this
+    const isRelativeBlock = _this.isRelativeBlock(targetFrame)
+    const viewportWidth = rectContext?.viewportWidth
+    const viewportHeight = rectContext?.viewportHeight
+
+    if (operation in frameOperations) {
+        switch (operation) {
+            case frameOperations.in:
+                setTimeout(() => {
+                    targetFrame.style.setProperty(cssVariable.width, `${viewportWidth}px`)
+                    targetFrame.style.setProperty(cssVariable.height, `${viewportHeight}px`)
+                    targetFrame.style.setProperty(cssVariable.x, `0`)
+                    targetFrame.style.setProperty(cssVariable.y, `0`)
+
+                    //targetFrame.classList.add(cssClasses.inZooming)
+                }, 0)
+                break;
+            case frameOperations.out:
+                setTimeout(() => {
+                    targetFrame.style.setProperty(cssVariable.width, _this.innerUtils.stringUnitsRemove(`${targetFrame.dataset.framemetaWidth}px`))
+                    targetFrame.style.setProperty(cssVariable.height, _this.innerUtils.stringUnitsRemove(`${targetFrame.dataset.framemetaHeight}px`))
+                    targetFrame.style.setProperty(cssVariable.x, _this.innerUtils.stringUnitsRemove(`${targetFrame.dataset.framemetaX}px`))
+                    targetFrame.style.setProperty(cssVariable.y,_this.innerUtils.stringUnitsRemove( `${targetFrame.dataset.framemetaY}px`))
+                    
+                }, 0)
+
+                break;
+            default:
+                break
+        }
+    }
+}
+
+_zooming.prototype.toggleZoomInOut = 
 
 _zooming.prototype.toggleZoomInOut = function (targetFrame: HTMLElement, operation: frameOperations) {
     const _this: Zooming = this
     console.log('toggleZoomInOut')
-    const isRelativeBlock = targetFrame.style.position === _this.styleNotAbsolute.emply
-        || targetFrame.style.position === _this.styleNotAbsolute.relative
-        || _this.relativeFrameInRegister.has(targetFrame)
+    const isRelativeBlock = _this.isRelativeBlock(targetFrame)
 
     if (isRelativeBlock) {
         _this.relativeFrameInRegister.set(targetFrame, {})
@@ -155,12 +221,12 @@ _zooming.prototype.toggleZoomInOut = function (targetFrame: HTMLElement, operati
         switch (operation) {
             case frameOperations.in:
                 if (isRelativeBlock) {
-                    const wrapper = targetFrame.closest('.zooming-frame-wrapper')
+                    //const wrapper = targetFrame.closest('.zooming-frame-wrapper')
 
                     const wrapperRect = _this.getDynamicRootClientRect(targetFrame)
                     const targetRect = targetFrame.getBoundingClientRect()
 
-                    
+
 
                     //calc
                     const top = targetRect.top - wrapperRect.top
@@ -174,42 +240,58 @@ _zooming.prototype.toggleZoomInOut = function (targetFrame: HTMLElement, operati
                     targetFrame.style.setProperty(cssVariable.y, `${top}px`)
 
                     let newDiv = _this.createDummyDiv()
-                    const dims:fullHtmlDimensions = {width, height, top, left}
+                    const dims: fullHtmlDimensions = { width, height, top, left }
 
                     newDiv = _this.insertDummy(newDiv, targetFrame, dims)
 
                     console.log("isRelativeBlock:in", top, left)
-                    
-
-                    // _this.setInitialCssAttribute(targetFrame, dims)
-                    
 
 
-                    
-
+                    _this.setInitialCssAttribute(targetFrame, dims)
 
                     targetFrame.classList.add(cssClasses.relative2Absolute)
 
 
-                    // targetFrame.classList.add(cssClasses.inZooming)
-                    // targetFrame.classList.remove(cssClasses.outZooming)
+                    targetFrame.classList.add(cssClasses.inZooming)
+                    targetFrame.classList.remove(cssClasses.outZooming)
 
                     // setInitialCssAttrible
                     // calc the corresponding top & left for absolute position, calc the width & height for dummy
                     // fill the original space with dummy div && turn the real one's position absolute 
-                    
-                }else{
+
+                } else {
                     targetFrame.classList.add(cssClasses.inZooming)
                     targetFrame.classList.remove(cssClasses.outZooming)
                 }
-                
+
                 break
             case frameOperations.out:
                 if (isRelativeBlock) {
                     // reverse
+                    //_this.removeDummy(targetFrame)//too soon
+
+                    if(_this.endTransistionHandle.has(targetFrame)){
+                        targetFrame.removeEventListener('transitionend', _this.endTransistionHandle.get(targetFrame))
+                        _this.endTransistionHandle.delete(targetFrame)
+                    }
+                    const transitionendHandle = targetFrame.addEventListener('transitionend', () => {
+                        targetFrame.classList.remove(cssClasses.relative2Absolute)
+                        _this.removeDummy(targetFrame)
+                        targetFrame.classList.remove(cssClasses.outZooming)
+                        targetFrame.classList.remove(cssClasses.inZooming)
+
+                        targetFrame.style.setProperty(cssVariable.x, `unset`)
+                        targetFrame.style.setProperty(cssVariable.y, `unset`)
+                    });
+
+                    _this.endTransistionHandle.set(targetFrame, transitionendHandle)
+
+                    
+                }else{
+                    targetFrame.classList.add(cssClasses.outZooming)
+                    targetFrame.classList.remove(cssClasses.inZooming)
                 }
-                targetFrame.classList.add(cssClasses.outZooming)
-                targetFrame.classList.remove(cssClasses.inZooming)
+
                 break
             default:
                 break
@@ -225,29 +307,16 @@ _zooming.prototype.in = function (target: HTMLDivElement) {
     const targetFrame: HTMLElement = target
 
     _this.toggleZoomInOut(targetFrame, frameOperations.in)
+    _this.toggleZoomInOut$Deferred(targetFrame, frameOperations.in, { viewportWidth, viewportHeight })
 
-    //deferred for render life cycle
-    setTimeout(()=>{ 
-        targetFrame.style.setProperty(cssVariable.width, `${viewportWidth}px`)
-        targetFrame.style.setProperty(cssVariable.height, `${viewportHeight}px`)
-        targetFrame.style.setProperty(cssVariable.x, `0`)
-        targetFrame.style.setProperty(cssVariable.y, `0`)
-    }, 0)
-    
-
-    targetFrame.classList.add(cssClasses.inZooming)
 }
 
 _zooming.prototype.out = function (target: HTMLDivElement) {
+    const _this: Zooming = this
     const targetFrame: HTMLElement = target
-    this.toggleZoomInOut(targetFrame, frameOperations.out)
+    _this.toggleZoomInOut(targetFrame, frameOperations.out)
+    _this.toggleZoomInOut$Deferred(targetFrame, frameOperations.out)
 
-    targetFrame.style.setProperty(cssVariable.width, `${targetFrame.dataset.framemetaWidth}`)
-    targetFrame.style.setProperty(cssVariable.height, `${targetFrame.dataset.framemetaHeight}`)
-    targetFrame.style.setProperty(cssVariable.x, `${targetFrame.dataset.framemetaX}`)
-    targetFrame.style.setProperty(cssVariable.y, `${targetFrame.dataset.framemetaY}`)
-
-    console.log(targetFrame)
 }
 
 export const zooming: Zooming = new _zooming()
