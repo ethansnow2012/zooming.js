fc6ada7b29e7b42e4a516ffecb77a3d8bd222f77
Ethansnow2012 <ethansnow2012@gmail.com>
2022-06-08 12:35:16 +0800

    chore: setting up vite && playground

---
+++ playground/react/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-restricted-globals */
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div>
+        Jsx can be executed here.
+    </div>
+  );
+};
+
+export default App;
